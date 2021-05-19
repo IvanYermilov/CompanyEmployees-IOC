d40245473b23375936c0c965e77de4f7355996a9
Ivan Yermilov <van.ermilov@gmail.com>
2021-05-20 00:27:08 +0300

    project creation

---
+++ IOCcontainer/IOCcontainer/Container.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOCcontainer
+{
+    class Container
+    {
+        public Dictionary<Type, Type> services; 
+    }
+}
+ 
---
+++ IOCcontainer/IOCcontainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOCcontainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
