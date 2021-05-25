559c89a7e44ea18a01be1562c16088369aaa2428
IvanYermilov <72105291+IvanYermilov@users.noreply.github.com>
2021-05-25 12:16:33 +0300

    Added test class

---
+++ IOCcontainer/IOCcontainer/MyClass4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOCcontainer
+{
+    class MyClass4
+    {
+        MyClass2 MC;
+
+        public MyClass4(MyClass2 MC)
+        {
+            this.MC = MC;
+        }
+    }
+}
--- IOCcontainer/IOCcontainer/Program.cs
+++ IOCcontainer/IOCcontainer/Program.cs
@@ -8,10 +8,12 @@ namespace IOCcontainer
         {
             Container.Add<IDependency3, MyClass3>();
             Container.Add<IDependency1, MyClass1>();
-            Container.Add<MyClass2, MyClass2>();
+            //Container.Add<MyClass2, MyClass2>();
+            Container.Add<MyClass4, MyClass4>();
             var inst1 = Container.GetImplementation<IDependency3>();
             var inst2 = Container.GetImplementation<IDependency1>();
             var inst3 = Container.GetImplementation<MyClass2>();
+            var inst4 = Container.GetImplementation<MyClass4>();
         }
     }
 }
