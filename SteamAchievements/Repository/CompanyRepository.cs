695c9e824548aa1d4ad2a71c1bacc6af539d0ddc
Ivan Yermilov <van.ermilov@gmail.com>
2021-05-31 17:46:41 +0300

    Implemented Repository Pattern

--- SteamAchievements/Repository/CompanyRepository.cs
+++ SteamAchievements/Repository/CompanyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Contracts;
 using Entities;
@@ -13,5 +14,9 @@ namespace Repository
             : base(repositoryContext)
         {
         }
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
     }
 }
--- SteamAchievements/SteamAchievements/Startup.cs
+++ SteamAchievements/SteamAchievements/Startup.cs
@@ -39,6 +39,7 @@ namespace SteamAchievements
             services.ConfigureRepositoryManager();
             services.AddControllers();
             services.ConfigureSqlContext(Configuration);
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
