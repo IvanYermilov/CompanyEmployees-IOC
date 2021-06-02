f757e7a036923b8912bfc72104d988a6c6e09a08
Ivan Yermilov <van.ermilov@gmail.com>
2021-06-02 09:34:40 +0300

    Enhanced employee controller + partially implemented content negotiation

--- SteamAchievements/SteamAchievements/Controllers/EmployeesController.cs
+++ SteamAchievements/SteamAchievements/Controllers/EmployeesController.cs
@@ -32,7 +32,7 @@ namespace SteamAchievements.Controllers
             var company = _repository.Company.GetCompany(companyId, trackChanges: false);
             if (company == null)
             {
-                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the бю database."); 
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database."); 
                 return NotFound();
             }
 
@@ -41,6 +41,28 @@ namespace SteamAchievements.Controllers
             return Ok(employeesDto);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database."); 
+                return NotFound();
+            }
+
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges:
+                false);
+            if (employeeDb == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+
+            return Ok(employee);
+        }
     }
 
 }
--- SteamAchievements/SteamAchievements/Startup.cs
+++ SteamAchievements/SteamAchievements/Startup.cs
@@ -39,8 +39,15 @@ namespace SteamAchievements
             services.ConfigureLoggerService();
             services.ConfigureRepositoryManager();
             services.AddControllers();
+
             services.ConfigureSqlContext(Configuration);
             services.AddAutoMapper(typeof(Startup));
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
