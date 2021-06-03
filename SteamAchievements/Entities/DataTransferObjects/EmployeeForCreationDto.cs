23ce4045e0bf867515129fd5fe6d9e3dbdc35cac
IvanYermilov <72105291+IvanYermilov@users.noreply.github.com>
2021-06-03 19:06:40 +0300

    Add capability to: create employees, get several companies

--- SteamAchievements/Entities/DataTransferObjects/EmployeeForCreationDto.cs
+++ SteamAchievements/Entities/DataTransferObjects/EmployeeForCreationDto.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace Entities.DataTransferObjects
 {
-    public class CompanyDto
+    public class EmployeeForCreationDto
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
-        public string FullAddress { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
     }
 }
--- SteamAchievements/SteamAchievements/MappingProfiles/MappingProfile.cs
+++ SteamAchievements/SteamAchievements/MappingProfiles/MappingProfile.cs
@@ -20,6 +20,8 @@ namespace SteamAchievements.MappingProfiles
             CreateMap<Employee, EmployeeDto>();
 
             CreateMap<CompanyForCreationDto, Company>();
+
+            CreateMap<EmployeeForCreationDto, Employee>();
         }
 
     }
