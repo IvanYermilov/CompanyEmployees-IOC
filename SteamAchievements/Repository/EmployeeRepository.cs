6dcda572111d0107d265f2653a95d7807adcdd76
IvanYermilov <72105291+IvanYermilov@users.noreply.github.com>
2021-06-12 12:24:43 +0300

    Inplemented employee searcging by name

--- SteamAchievements/Repository/EmployeeRepository.cs
+++ SteamAchievements/Repository/EmployeeRepository.cs
@@ -8,6 +8,7 @@ using Entities;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -20,9 +21,11 @@ namespace Repository
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
             EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) &&
-            e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge, trackChanges)
-                .OrderBy(e => e.Name)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+                .Search(employeeParameters.SearchTerm)
+
+                        .OrderBy(e => e.Name)
                 .ToListAsync(); 
 
             return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
