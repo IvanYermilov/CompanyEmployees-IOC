d05f3a920103d7609355491483622c4e483c859b
IvanYermilov <72105291+IvanYermilov@users.noreply.github.com>
2021-06-12 11:47:06 +0300

    Implemenred filtering for employee

--- SteamAchievements/SteamAchievements/Controllers/EmployeesController.cs
+++ SteamAchievements/SteamAchievements/Controllers/EmployeesController.cs
@@ -33,6 +33,9 @@ namespace SteamAchievements.Controllers
         [HttpGet]
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, [FromQuery]EmployeeParameters employeeParameters)
         {
+            if (!employeeParameters.ValidAgeRange)
+                return BadRequest("Max age can't be less than min age.");
+
             var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
@@ -40,7 +43,7 @@ namespace SteamAchievements.Controllers
                 return NotFound();
             }
 
-            var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId,employeeParameters, trackChanges: false);
+            var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges: false);
             Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(employeesFromDb.MetaData));
             var  employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
             return Ok(employeesDto);
