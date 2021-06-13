cb0972739eee589908ac3a661ce195c5c6ea74a7
Ivan Yermilov <van.ermilov@gmail.com>
2021-06-13 16:37:43 +0300

    Implemented sorting for employees

--- SteamAchievements/Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ SteamAchievements/Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -2,13 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
 using System.Text;
+using SteamAchievements.Extensions.Utility;
 
 namespace Repository.Extensions
 {
     public static class RepositoryEmployeeExtensions
     {
-        public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
+        public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge,
+            uint maxAge) =>
             employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
 
         public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string searchTerm)
@@ -19,6 +23,19 @@ namespace Repository.Extensions
             var lowerCaseTerm = searchTerm.Trim().ToLower();
 
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return employees.OrderBy(e => e.Name);
+
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+
+            return employees.OrderBy(orderQuery);
         }
     }
 }
